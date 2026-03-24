1eca493d6a8d0ebd74f28ccb5a7bf3d5b69c32b8
Serge Kozlov <serge.kozlov.dev@gmail.com>
2026-03-24 11:51:08 +0200

    pz_1 complete

--- pz_1/practical_lesson_1.py
+++ pz_1/practical_lesson_1.py
@@ -1,1 +1,37 @@
-print("init commit")
+from datetime import datetime
+# task 1
+print("===TASK1===")
+print(*range(1, 11))
+print("===TASK1===")
+
+
+
+#task 2
+print("===TASK2===")
+print(sum(float(input()) for _ in range(3)) / 3)
+print("===TASK2===")
+
+#task 3
+print("===TASK3===")
+year = int(input("Введіть день народження: "))
+current_year = datetime.now().year
+
+print(current_year - year)
+print("===TASK3===")
+
+#task 4
+print("===TASK4===")
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+
+book1 = Book("1984", "George Orwell", 1949)
+
+print("Назва:", book1.title)
+print("Автор:", book1.author)
+print("Рік:", book1.year)
+print("===TASK4===")
+
